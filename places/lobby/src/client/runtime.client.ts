0f0e9e4a48f12962cbc1bfd5555440c92f677a30
OminousVibes <ominousvibes.development@gmail.com>
2023-05-15 18:12:02 -0400

    chore: Change EOL Sequence to LF (Standard)

--- places/lobby/src/client/runtime.client.ts
+++ places/lobby/src/client/runtime.client.ts
@@ -1,11 +1,6 @@
 import { Flamework } from "@flamework/core";
 
 Flamework.addPaths("places/lobby/src/client/controllers");
-Flamework.addPaths("places/common/src/client/controllers");
-
 Flamework.addPaths("places/lobby/src/client/components");
 Flamework.addPaths("places/lobby/src/shared/components");
-Flamework.addPaths("places/common/src/client/components");
-Flamework.addPaths("places/common/src/shared/components");
-
 Flamework.ignite();
