9ba59db0cdd815d2b386e0d6a3bd3b53e1ca44ac
OminousVibes <ominousvibes.development@gmail.com>
2023-05-15 18:05:11 -0400

    chore: made controllers/components in /common/ opt in

--- places/multiplayer/src/server/runtime.server.ts
+++ places/multiplayer/src/server/runtime.server.ts
@@ -1,11 +1,6 @@
 import { Flamework } from "@flamework/core";
 
-Flamework.addPaths("places/lobby/src/server/controllers");
-Flamework.addPaths("places/common/src/server/controllers");
-
-Flamework.addPaths("places/lobby/src/server/components");
-Flamework.addPaths("places/lobby/src/shared/components");
-Flamework.addPaths("places/common/src/server/components");
-Flamework.addPaths("places/common/src/shared/components");
-
+Flamework.addPaths("places/multiplayer/src/server/controllers");
+Flamework.addPaths("places/multiplayer/src/server/components");
+Flamework.addPaths("places/multiplayer/src/shared/components");
 Flamework.ignite();
